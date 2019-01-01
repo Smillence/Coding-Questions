8ab1941f5d356be6c64aed47a2faf1dd54121483
Zidong <smillence1992@gmail.com>
2019-01-01 15:32:38 -0800

    1.1 is_unique

--- Cracking_the_coding_interview/1.1.py
+++ Cracking_the_coding_interview/1.1.py
@@ -1,5 +1,5 @@
 '''Question:
-implement an algorithm to determine if a string has all unique characters. 
+implement an algorithm to determine if a string has all unique characters.
 What if you can not use additional data structures?
 '''
 
@@ -7,22 +7,34 @@ What if you can not use additional data structures?
 Confusions:
 1. What do you mean by "all unique characters"? I assume it is ACSII string with
  value 0~255.
-2. What do you mean by "cannot use addtional data structures"? I assume I can 
-only use string.
-3. Does the string need to include all 256 characters? Or just need to have no 
+2. What do you mean by "cannot use addtional data structures"?
+3. Does the string need to include all 256 characters? Or just need to have no
 duplicates. (assume the later one)
 Examples:
 1) agshdhfkd -> False
 2) agsdhfk -> True
 '''
 
-# Idea: hash table
+
+# Solution: hash table
 # time: O(n)
-# space: O(1) - fixed size 256
-def all_unique_character_1(string):
+# space: O(1) - Note: there are only 256 characters
+def is_unique_1(string):
+    s = set()
+    for char in string:
+        if char in s:
+            return False
+        else:
+            s.add(char)
+    return True
+
+# Solution: similar to hash table
+# time: O(n)
+# space: O(1)
+def is_unique_2(string):
     table = [0 for x in range(256)]
     for char in string:
-        # ord returns the integer ordinal of a one-character string
+        # returns the integer ordinal of a character
         index = ord(char)
         if table[index] == 0:
             table[index] = 1
@@ -30,45 +42,19 @@ def all_unique_character_1(string):
             return False
     return True
 
-# Idea: hash table
-# time: ?
-# space: O(1)
-def all_unique_character_2(string):
-    table = []
-    for char in string:
-        if char in table:
-            return False
-        table.append(char)
-    return True
-
-# Idea: use bit vector, since Mac OS is in 64 bit, we need 4 vectors
+# Solution: bit vector
+# Limitation: using int as bit vector will be very slow if there are millions of bits
 # time: O(n)
 # space: O(1)
-def all_unique_character_3(string):
-    checker_1 = checker_2 = checker_3 = checker_4 = 0
+def is_unique_3(string):
+    checker = 0
     for char in string:
-        order = ord(char)
-        if order < 64:
-            if (checker_1 & (1<<order)) != 0:
-                return False
-            checker_1 |= 1<<order
-        elif order < 128:
-            order -= 64
-            if (checker_2 & (1<<order)) != 0:
-                return False
-            checker_2 |= 1<<order
-        elif order < 192:
-            order -= 128
-            if (checker_3 & (1<<order)) != 0:
-                return False
-            checker_3 |= 1<<order
+        pos = ord(char)
+        if (checker >> pos) & 1:
+            return False
         else:
-            order -= 192
-            if (checker_4 & (1<<order)) != 0:
-                return False
-            checker_4 |= 1<<order
+            checker |= 1 << pos
     return True
-
 
 ''' Below are Unit Tests '''
 
@@ -106,11 +92,11 @@ def unit_test_4(func):
 
 def run_unit_tests(func):
     unit_test_1(func)
-    unit_test_2(func)  
-    unit_test_3(func)  
+    unit_test_2(func)
+    unit_test_3(func)
     unit_test_4(func)
 
 if __name__ == '__main__':
-    run_unit_tests(all_unique_character_1)
-    run_unit_tests(all_unique_character_2)
-    run_unit_tests(all_unique_character_3)
+    run_unit_tests(is_unique_1)
+    run_unit_tests(is_unique_2)
+    run_unit_tests(is_unique_3)
