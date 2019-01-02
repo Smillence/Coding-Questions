d07e7b7a51babc4e6beea888734490059e617ef8
Zidong <smillence1992@gmail.com>
2019-01-01 17:58:01 -0800

    1.2 use collections.Counter

--- Cracking_the_coding_interview/v6/1.2_check_permutation.py
+++ Cracking_the_coding_interview/v6/1.2_check_permutation.py
@@ -3,12 +3,12 @@ Given two strings, write a method to decide if one is a permutation of the
 other.
 '''
 import unittest
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 # Solution: hash table
 # time: O(n)
 # space: O(1) - there are only 256 possible keys
-def check_permutation(s1, s2):
+def check_permutation_1(s1, s2):
     if len(s1) != len(s2):
         return False
     s1_char_count = defaultdict(int)
@@ -19,6 +19,12 @@ def check_permutation(s1, s2):
             return False
         s1_char_count[char] -= 1
     return True
+
+# Solution: same as 1. but use Counter
+def check_permutation_2(s1, s2):
+    if len(s1) != len(s2):
+        return False
+    return Counter(s1) == Counter(s2)
 
 class TestCheckPermutation(unittest.TestCase):
     def data_provider(self):
@@ -35,14 +41,21 @@ class TestCheckPermutation(unittest.TestCase):
             ('aabc', 'bacd', False),
         ]
 
+    def func_provider(self):
+        return [
+            check_permutation_1,
+            check_permutation_2,
+        ]
+
     def test(self):
-        for s1, s2, expected in self.data_provider():
-            output = check_permutation(s1, s2)
-            self.assertEqual(
-                output,
-                expected,
-                "check_permutation('%s', '%s') output: %r. Expect: %r" % (s1, s2, output, expected),
-            )
+        for func in self.func_provider():
+            for s1, s2, expected in self.data_provider():
+                output = func(s1, s2)
+                self.assertEqual(
+                    output,
+                    expected,
+                    "%s('%s', '%s') output: %r. Expect: %r" % (func.__name__, s1, s2, output, expected),
+                )
 
 if __name__ == '__main__':
     unittest.main()
